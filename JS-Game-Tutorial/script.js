029db20892b45d67e3e1d6f4457ed26e36f82223
ckirkwood94 <ckirkwood94@gmail.com>
2023-03-13 13:17:55 -0400

    Added skeleton classes for player, enemy, and game

--- JS-Game-Tutorial/script.js
+++ JS-Game-Tutorial/script.js
@@ -2,3 +2,35 @@
   // Optional code that runs immediately
   console.log('Hello World');
 })();
+
+class Player {
+  constructor(level, health, weapon, coords, xp) {
+    this.level = level;
+    this.health = health;
+    this.weapon = weapon;
+    this.coords = coords;
+    this.xp = xp;
+  }
+}
+
+class Enemy {
+  constructor(health, coords, damage) {
+    this.health = health;
+    this.coords = coords;
+    this.damage = damage;
+  }
+}
+
+class Game {
+  constructor() {
+    this.map = [];
+    this.enemies = [];
+    this.canvas = null;
+    this.context = null;
+  }
+}
+
+Game.prototype.reset = function () {
+  this.enemies = [];
+  this.map = [];
+};
